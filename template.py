5678898fe0c74f7511ab8070305c26d0f328f180
Elio <nguyen2332004@gmail.com>
2025-03-08 14:22:30 +0700

    initialize logging configuration and set project name in template.py

---
+++ template.py
@@ -0,0 +1,11 @@
+import os 
+from pathlib import Path
+import logging
+
+# logging string
+logging.basicConfig(Level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = 'cnnClassifier'
+
+
+
