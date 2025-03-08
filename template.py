d2625f8990421307d84b17338f2089e565442f86
Elio <nguyen2332004@gmail.com>
2025-03-08 14:35:07 +0700

    create directories and empty files for listed paths in template.py

--- template.py
+++ template.py
@@ -26,3 +26,18 @@ list_of_files = [
     "templates/index.html"
 ]
 
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir !="":
+        os.makeddirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exits(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "W") as f: 
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+    else: 
+        logging.info(f"{filename} is already  exists")
